15cab4bbbdee936d7999f10c09aadd8a6f3c6014
thanhphat219 <phat01283249260@gmail.com>
2022-05-14 21:29:07 +0700

    add room

---
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/WordRepository.java
@@ -0,0 +1,29 @@
+package com.example.roomwordsample;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+class WordRepository {
+
+    private final WordDao mWordDao;
+    private final LiveData<List<Word>> mAllWords;
+
+    WordRepository(Application application) {
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAlphabetizedWords();
+    }
+
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    void insert(Word word) {
+        WordRoomDatabase.databaseWriteExecutor.execute(() -> {
+            mWordDao.insert(word);
+        });
+    }
+}
